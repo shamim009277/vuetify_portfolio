b4c2d226bc4ce4cd50387e003a43d01cf4b0ac01
Towhedul <towhidshamim009@gmail.com>
2025-02-03 16:56:57 +0600

    change config file

--- vue.config.js
+++ vue.config.js
@@ -4,10 +4,6 @@ module.exports = defineConfig({
 
   pluginOptions: {
     vuetify: {
-			base: '/vuetify_portfolio',  // এখানে 'repository-name' আপনার GitHub রিপোজিটরির নাম হবে
-      build: {
-        outDir: 'dist',  // ভিউ প্রোজেক্টের বিল্ড ফোল্ডার
-      },
 		}
   }
 })
