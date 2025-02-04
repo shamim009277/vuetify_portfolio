98939a7f371843df9735ae5fc542176b2f93ce16
Towhedul <towhidshamim009@gmail.com>
2025-02-04 14:32:23 +0600

    url update for deploy

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/vuetify_portfolio/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/vuetify-portfolio/' : '/',
   outputDir: 'dist',
   pluginOptions: {
     vuetify: {
